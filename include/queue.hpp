eb3c8b674bcca3affb369d41f646bc45427ac630
Charlie Schoettle <cschoettle981@gmail.com>
2025-02-21 20:42:20 -0500

    removed namespace from queue header

--- include/queue.hpp
+++ include/queue.hpp
@@ -4,20 +4,18 @@
 #include "config.hpp"
 #include <protocols.hpp>
 
-using namespace Sensor;
-
 class Queue {
 private:
-    SensorPacket_t arr[DATA_QUEUE_LENGTH] = {0};
+    Telemetry::SensorPacket_t arr[Telemetry::DATA_QUEUE_LENGTH] = {0};
     uint32_t size;
     uint32_t front;
     uint32_t back;
     uint32_t count;
 
 public:
-    Queue() : size(DATA_QUEUE_LENGTH), front(0), back(0), count(0) {}
-    void enqueue(SensorPacket_t value);
-    SensorPacket_t dequeue();
+    Queue() : size(Telemetry::DATA_QUEUE_LENGTH), front(0), back(0), count(0) {}
+    void enqueue(Telemetry::SensorPacket_t value);
+    Telemetry::SensorPacket_t dequeue();
     bool is_empty();
     uint32_t get_size();
 };
