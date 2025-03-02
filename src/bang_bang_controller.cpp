019527113b64fe1ad2b0b47de1f53a1b5e18b6cd
Charlie Schoettle <cschoettle981@gmail.com>
2025-03-02 18:19:02 -0500

    use chrono for bb controller

--- src/bang_bang_controller.cpp
+++ src/bang_bang_controller.cpp
@@ -7,10 +7,16 @@ extern "C" {
 #include <stdbool.h>
 }
 
+#include <chrono>
+#include <thread>
+
 using namespace BB_State;
+using namespace std::chrono;
 
 State BB_State::bb_fu_state = State::ISOLATE;
 State BB_State::bb_ox_state = State::ISOLATE;
+int   BB_State::bb_fu_pos   = BB_Constants::BB_CLOSE;
+int   BB_State::bb_ox_pos   = BB_Constants::BB_CLOSE;
 
 uint64_t BB_State::bb_fu_upper_setp = UINT64_MAX;
 uint64_t BB_State::bb_fu_lower_setp = UINT64_MAX - 1;
@@ -21,8 +27,8 @@ void* bang_bang_controller(void* arg) {
     struct sched_param param;
     param.sched_priority = 99; // highest prio
     pthread_setschedparam(pthread_self(), SCHED_RR, &param);
-
     // TODO: FDIR
+
     struct gpiod_chip *bb_chip;
     struct gpiod_line *bb_fu_line;
     struct gpiod_line *bb_ox_line;
@@ -47,39 +53,37 @@ void* bang_bang_controller(void* arg) {
     if (gpiod_line_request_output(bb_ox_line, "test_ox?", BB_Constants::BB_CLOSE) < 0) {
         // TODO: FDIR
     }
+
+    auto fu_last_set = std::chrono::steady_clock::now();
+    auto ox_last_set = fu_last_set;
+
+    int intended_fu_pos = BB_Constants::BB_CLOSE;
+    int intended_ox_pos = BB_Constants::BB_CLOSE;
     
     while (true) {
-        struct timespec time;
-        clock_gettime(CLOCK_MONOTONIC, &time);
-        // TODO: FDIR
-        time.tv_nsec += BB_Constants::TICK_RATE_MS * 1000000;
-        if (time.tv_nsec >= 1000000000) {
-            time.tv_sec += 1;
-            time.tv_nsec -= 1000000000;
-        }
-
-        int new_fu_state;
-        int new_ox_state;
+        auto now = time_point_cast<microseconds>(steady_clock::now());
 
         int curr_fu_pressure = Telemetry::fu_pressure;
         int curr_ox_pressure = Telemetry::ox_pressure;
+        State curr_fu_state  = bb_fu_state;
+        State curr_ox_state  = bb_ox_state;
 
         // find new FU state
         switch (bb_fu_state) {
             case REGULATE: {
                 if ((curr_fu_pressure >= bb_fu_upper_setp)) {
-                    new_fu_state = BB_Constants::BB_CLOSE;
+                    intended_fu_pos = BB_Constants::BB_CLOSE;
                 } else if (curr_fu_pressure <= bb_fu_lower_setp) {
-                    new_fu_state = BB_Constants::BB_OPEN;
+                    intended_fu_pos = BB_Constants::BB_OPEN;
                 }
                 break;
             }
             case ISOLATE: {
-                new_fu_state = BB_Constants::BB_CLOSE;
+                intended_fu_pos = BB_Constants::BB_CLOSE;
                 break;
             }
             case OPEN: {
-                new_fu_state = BB_Constants::BB_OPEN;
+                intended_fu_pos = BB_Constants::BB_OPEN;
                 break;
             }
         }
@@ -88,30 +92,39 @@ void* bang_bang_controller(void* arg) {
         switch (bb_ox_state) {
             case REGULATE: {
                 if ((curr_ox_pressure >= bb_ox_upper_setp)) {
-                    new_ox_state = BB_Constants::BB_CLOSE;
+                    intended_ox_pos = BB_Constants::BB_CLOSE;
                 } else if (curr_ox_pressure <= bb_ox_lower_setp) {
-                    new_ox_state = BB_Constants::BB_OPEN;
+                    intended_ox_pos = BB_Constants::BB_OPEN;
                 }
                 break;
             }
             case ISOLATE: {
-                new_ox_state = BB_Constants::BB_CLOSE;
+                intended_ox_pos = BB_Constants::BB_CLOSE;
                 break;
             }
             case OPEN: {
-                new_ox_state = BB_Constants::BB_OPEN;
+                intended_ox_pos = BB_Constants::BB_OPEN;
                 break;
             }
         }
-        
-        if (gpiod_line_set_value(bb_fu_line, new_fu_state) < 0) {
+
+        if (now >= (fu_last_set + milliseconds(50)) && bb_fu_pos != intended_fu_pos) {
+            bb_fu_pos   = intended_fu_pos;
+            fu_last_set = now;
+        }
+
+        if (now >= (ox_last_set + milliseconds(50)) && bb_fu_state != intended_ox_pos) {
+            bb_ox_pos   = intended_ox_pos;
+            ox_last_set = now;
+        }
+
+        if (gpiod_line_set_value(bb_fu_line, intended_fu_pos) < 0) {
             // TODO: FDIR
         }
-        if (gpiod_line_set_value(bb_ox_line, new_ox_state) < 0) {
+        if (gpiod_line_set_value(bb_ox_line, intended_ox_pos) < 0) {
             // TODO: FDIR
         }
-        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &time, NULL);
-        // TODO: FDIR
+        std::this_thread::sleep_until(now + std::chrono::milliseconds(BB_Constants::TICK_RATE_MS));
     }
 
     gpiod_line_release(bb_fu_line);
