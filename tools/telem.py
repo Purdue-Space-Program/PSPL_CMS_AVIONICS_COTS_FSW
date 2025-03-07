ce132e59ef57faada2984b1bd9667feebcf5233f
Charlie Schoettle <cschoettle981@gmail.com>
2025-03-06 21:45:54 -0500

    telem.py now reads in chunks for better performance

--- tools/telem.py
+++ tools/telem.py
@@ -1,34 +1,71 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from enum import IntEnum
-from time import sleep
 from struct import unpack, calcsize
 import synnax as sy
+import time
 
 client = sy.Synnax(
    host="128.46.118.59", port=9090, username="Bill", password="Bill", secure=False
 )
 
-TELEM_FORMAT = '<QQB7x'
+TELEM_FORMAT = '<QQQ'
 TELEM_SIZE   = calcsize(TELEM_FORMAT)
+
+timestamp_channel = client.channels.create(
+    name="adc_time",
+    data_type=sy.DataType.TIMESTAMP,
+    retrieve_if_name_exists=True,
+    is_index=True,
+)
+
+other_channel = client.channels.create(
+    name="adc_val",
+    data_type=sy.DataType.FLOAT64,
+    retrieve_if_name_exists=True,
+    index=timestamp_channel.key,
+)
 
 if __name__ == '__main__':
     with socket(AF_INET, SOCK_STREAM) as s:
         s.connect(('192.168.1.147', 25565))
-
+        
+        start_time = sy.TimeStamp.now()
         with client.open_writer(
-            start=sy.TimeStamp.now(), 
+            start=start_time,
             channels=["adc_time", "adc_val"],
             enable_auto_commit=True,
         ) as writer:
-            
-            while True:
+            try:
+                # get single packet
                 packet = s.recv(TELEM_SIZE)
-                if len(packet) == TELEM_SIZE:
-                    deser_packet = unpack(TELEM_FORMAT, packet)
-                    
-                    if deser_packet[2] == 2:
-                        print((((deser_packet[2]/float(2**31)) * 5) * 0.958241 + 0.0146882) * 187)
-                    writer.write({
-                        "adc_time": deser_packet[0],
-                        "adc_val": deser_packet[1],
-                    })
+                while (len(packet) != TELEM_SIZE):
+                    packet = s.recv(TELEM_SIZE)
+
+                offset = start_time + packet[0] * 1000
+
+                buff = b''
+                while True:
+                    packet = s.recv(1000)
+                    if packet: 
+                        buff += packet
+
+                    while len(buff) >= TELEM_SIZE:
+                        p = buff[:TELEM_SIZE]
+                        buff = buff[TELEM_SIZE:]
+
+                        if len(p) != TELEM_SIZE:
+                            print(len(p))
+                            break
+
+                        deser_packet = unpack(TELEM_FORMAT, p)
+                        
+                        if deser_packet[2] == 2:
+                            print((((deser_packet[2]/float(2**31)) * 5) * 0.958241 + 0.0146882) * 187)
+                            writer.write({
+                                "adc_time": offset + deser_packet[0] * 1000,
+                                "adc_val": ((((deser_packet[1]/float(2**31)) * 5) * 0.958241 + 0.0146882) * 1),
+                            })
+                            print(deser_packet)
+            except Exception as e:
+                # writer.close()
+                print(f'Server closed: {e}')
