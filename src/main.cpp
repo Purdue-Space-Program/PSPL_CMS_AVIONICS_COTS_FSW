5328928e0e6fbe3e41504a1772647d491a496045
Charlie Schoettle <cschoettle981@gmail.com>
2025-03-12 16:58:48 -0400

    added state_writer to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,14 @@ sem_t start_sem;
 int main() {
     sem_init(&start_sem, 0, 0);
 
-    void* (*funcs[])(void*) = { daq, command_handler, bang_bang_controller, data_writer };
-    pthread_t threads[4] = {0};
+    void* (*funcs[])(void*) = { daq, command_handler, bang_bang_controller, data_writer, state_writer };
+    pthread_t threads[5] = {0};
 
-    for (size_t i = 0; i < 4; i += 1) {
+    for (size_t i = 0; i < 5; i += 1) {
         pthread_create(threads + i, NULL, funcs[i], NULL);
     }
 
-    for (size_t i = 0; i < 4; i += 1) {
+    for (size_t i = 0; i < 5; i += 1) {
         pthread_join(threads[i], NULL);
     }
 
