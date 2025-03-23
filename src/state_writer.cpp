658280f2d1ce118d7c4e9a877a83472f4a151fc1
Charlie Schoettle <cschoettle981@gmail.com>
2025-03-22 21:10:04 -0400

    state_writer system clock and changed priority

--- src/state_writer.cpp
+++ src/state_writer.cpp
@@ -17,8 +17,8 @@ using namespace std::chrono;
 volatile uint64_t* state_telemetry[] = {
     (uint64_t*)&BB_State::bb_fu_state,
     (uint64_t*)&BB_State::bb_ox_state,
-    (uint64_t*)&BB_State::bb_fu_pos,
-    (uint64_t*)&BB_State::bb_ox_pos,
+    &BB_State::bb_fu_pos,
+    &BB_State::bb_ox_pos,
     &BB_State::bb_fu_upper_setp,
     &BB_State::bb_fu_lower_setp,
     &BB_State::bb_ox_upper_setp,
@@ -27,16 +27,16 @@ volatile uint64_t* state_telemetry[] = {
 
 void* state_writer(void* arg) {
     struct sched_param param;
-    param.sched_priority = 10; // highest prio
+    param.sched_priority = 11; // highest prio
     pthread_setschedparam(pthread_self(), SCHED_RR, &param);
     // TODO: FDIR
 
     while (true) {
-        auto now = steady_clock::now();
+        auto now = system_clock::now();
 
         for (size_t i = 0; i < Telemetry::NUM_STATE_CHANNELS; i += 1) {
             Telemetry::state_mutex.lock();
-            uint64_t n = duration_cast<microseconds>(steady_clock::now().time_since_epoch()).count();
+            uint64_t n = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
             Telemetry::data_queue.enqueue({
                 .timestamp = n,
                 .data = *(state_telemetry[i]),
