5d9233c9c3477c17962afbe8f626b9162cff6db8
Charlie Schoettle <cschoettle981@gmail.com>
2025-10-07 16:33:08 -0400

    reworked docker build process

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build the image and give it a name (e.g., "cmake-builder")
+docker build -t cmake-builder .
+
+# Run the container, execute the cmake commands, and then exit
+# The "build" directory will appear in your current folder.
+docker run --rm \
+    -v $(pwd):/work \
+    -w /work \
+    cmake-builder \
+    bash -c "cmake -S . -B build && cmake --build build"
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build .
-docker run -it --rm \
-    -v $(pwd):/work \
-    -w /work \
-    $(docker build -q .)
