ea6d96060db7f8905db8a3c1d1e7d1172d910b6b
Charlie Schoettle <cschoettle981@gmail.com>
2025-10-18 15:36:36 -0400

    fixed build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker build -q -t cmake-builder .
 # Run the container, execute the cmake commands, and then exit
 # The "build" directory will appear in your current folder.
 docker run --rm \
-    -v $(pwd):/work \
-    -w /work \
+    -v $(pwd):$(pwd) \
+    -w $(pwd) \
     cmake-builder \
     bash -c "cmake -S . -B build && cmake --build build"
